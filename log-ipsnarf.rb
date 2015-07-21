ea475b79b5c3a72e32eb57d1e06825cc3dc5f408
danagilliann <danalee96@gmail.com>
2015-07-21 15:32:06 -0400

    success with string

--- log-ipsnarf.rb
+++ log-ipsnarf.rb
@@ -1,68 +1,39 @@
-#!/usr/bin/env ruby
-#
-# log-ipsnarf.rb
-#
-# Small utility program to collect ip addresses and hostnames from logfiles. 
-# Also collects hostnames and finds the IP address of it using resolv.
-# Very useful if you want to collect IP addresses from IRC logs ;)
-#
-# Author: Gordon Robert Speirs <grs@xhdev[~dot~]com
-# http://www.xhdev.com
-#
+# Make a method for opening the input
+# Make a method for parsing the input
+  # Make a method for detecting duplicate IP addresses
 
 require 'resolv'
 
-def ipsnarf(inputfile, outputfile)
-  if !File.exists?(inputfile)
-    puts "log-ipsnarf: Input file does not exist!\n"
-    exit
-  end
+def main()
+  #input = File.open(log, 'r')
+  #output = File.open(output, 'a+')
+  input = ['180.33.45.2', '180.33.45.2' '1.9.332.4', '44.256.34.23']
+  output = {}
 
-  input = File.open(inputfile, 'r')
-  output = File.open(outputfile, 'a+')
+  match_input(input, output)
 
-  snarf_hosts(input, output)
-  snarf_ipaddr(input, output)
-
-  input.close
-  output.close
+  #check_input(input)
+  #open(input, output)
 end
 
-def snarf_hosts(input, output)
-  host_regex = /(?:[a-z0-9\-]+\.)+[a-z]+/
+#def check_input(input)
+#  if !File.exists?(input)
+#    puts 'The input file does not exist!'
+#    exit
+#  end 
+#end
 
-  input.each_line do |line|
-    next unless host = line.match(host_regex)
-    begin
-      ip = Resolv.getaddress("#{host}")
-    rescue
-      next
-    end
-    puts "#{ip}" # print to term for debugging
-    output.write("#{ip}\n")
+def match_input(input, output)
+  ip_regex = /\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}/
+
+  input.each do |element|
+    if element.match ip_regex
+      output[element] += 1
+    end 
   end
-end
+  puts output
+end  
 
-def snarf_ipaddr(input, output)
-  ip_addr_regex = /\b(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\b/
+main()
 
-  input.each_line do |line|
-    next unless ip_addr = line.match(ip_addr_regex)
-    puts "#{ip_addr}" # print to term for debugging
-    output.write("#{ip_addr}\n")
-  end
-end
-
-if __FILE__ == $PROGRAM_NAME
-  unless ARGV.length == 2
-    puts "Invalid argument length!"
-    puts "Usage: ruby log-ipsnarf.rb <inputfile> <outputfile>\n"
-    exit
-  end
-
-  inputfile = ARGV[0]
-  outputfile = ARGV[1]
-
-  ipsnarf(inputfile, outputfile)
-end
-
+  
