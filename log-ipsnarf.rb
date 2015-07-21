08b08f7195de65fb24cb33ab54d3f4daf4076881
danagilliann <danalee96@gmail.com>
2015-07-21 17:06:18 -0400

    PARSED

--- log-ipsnarf.rb
+++ log-ipsnarf.rb
@@ -1,18 +1,17 @@
-# Make a method for opening the input
 # Make a method for parsing the input
-  # Make a method for detecting duplicate IP addresses
 
-require 'resolv'
+require 'ipaddr'
 
 def main()
   input = File.open('log', 'r')
   #output = File.open(output, 'a+')
   output = {}
 
-  match_input(input, output)
+  check_input(input)
+  array = parsing_file(input)
+  puts array
+  #match_input(array, output)
   input.close
-  #check_input(input)
-  #open(input, output)
 end
 
 def check_input(input)
@@ -22,15 +21,24 @@ def check_input(input)
   end 
 end
 
+def parsing_file(input)
+  ip_regex = /\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}/
+  ip_list = []
+
+  input.each do |line|
+    ip_list << line.scan(ip_regex)
+  end
+
+  return ip_list
+end
+
 def match_input(input, output)
   count = 0
   ip_regex = /\d{1,3}\.\d{1,3}\.\d{1,3}.\d{1,3}/
 
   input.each do |element|
-    if element.match ip_regex
-      count += input.count(element)
-      output[element] = count
-    end 
+    count += input.count(element)
+    output[element] = count 
   end
   puts output
 end  
